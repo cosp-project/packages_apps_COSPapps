fdccad368858adc09d93dd4ba602d0173203d695
edi194 <misiakadrian@gmail.com>
2019-01-16 06:49:15 +0100

    cospapps: fix derp

--- Android.mk
+++ Android.mk
@@ -18,7 +18,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := Lawnchair
-LOCAL_SRC_FILES := $(LOCAL_MODULE).apk
+LOCAL_SRC_FILES := apk/$(LOCAL_MODULE).apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_OVERRIDES_PACKAGES := Home Launcher2 Launcher3 Launcher3QuickStep
